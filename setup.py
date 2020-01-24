b64e71acb4eb84254efded3c15fb4ce39942a1cb
Eric Yager <ericyager@Erics-MacBook-Air-2.local>
2020-01-24 14:23:57 -0600

    new directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DATA151 project about no passing zones in Porter County. Spring 2020.',
+    author='Eric Yager',
+    license='',
+)
