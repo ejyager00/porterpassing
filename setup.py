5efe1bf8363a1ddaff4c1255d8db3ff281e50c96
Eric Yager <eric.yager@valpo.edu>
2020-02-01 13:12:06 -0600

    update makefile to remove s3 syncing and update setup.py to include all names

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='DATA151 project about no passing zones in Porter County. Spring 2020.',
-    author='Eric Yager',
+    author='Eric Yager, Jordan Blohm, Cassandra McMorrow',
     license='',
 )
