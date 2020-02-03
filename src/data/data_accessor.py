c5c0ad2f9c9f831f1388656ff65b689d90f41bba
Eric Yager <eric.yager@valpo.edu>
2020-02-03 12:42:19 -0600

    clean up data_accessor.py and make_dataset.py

--- src/data/data_accessor.py
+++ src/data/data_accessor.py
@@ -1,31 +1,30 @@
+"""This module opens a connection to the remote storage location."""
 import os
 from base64 import decodebytes
-import pysftp
+from pysftp import Connection, CnOpts
 from paramiko.ecdsakey import ECDSAKey
-from dotenv import find_dotenv, load_dotenv
-#above line is only necessary if this is not run in tandem with make_dataset
+#from dotenv import find_dotenv, load_dotenv
+#above line is only necessary if this is not run in tandem with make_dataset.py
 
-load_dotenv(find_dotenv())
-#above line is only necessary if this is not run in tandem with make_dataset
+#load_dotenv(find_dotenv())
+#above line is only necessary if this is not run in tandem with make_dataset.py
 
 #-------LOAD ENVIRONMENT VARIABLES FOR CONNECTION--------#
-hostname = str(os.environ.get("DATA_ADDRESS"))
-username = str(os.environ.get("USERNAME"))
-password = str(os.environ.get("PASSWORD"))
-remote_path = str(os.environ.get("DIRECTORY_PATH"))
-key_type = str(os.environ.get("TYPE"))
-key = ECDSAKey(data=decodebytes(os.environ.get("KEY").encode()))
-
+HOSTNAME = str(os.environ.get("DATA_ADDRESS"))
+USERNAME = str(os.environ.get("USERNAME"))
+PASSWORD = str(os.environ.get("PASSWORD"))
+REMOTE_PATH = str(os.environ.get("DIRECTORY_PATH"))
+KEY_TYPE = str(os.environ.get("TYPE"))
+KEY = ECDSAKey(data=decodebytes(os.environ.get("KEY").encode()))
 #-------HOST KEY-----#
-cnopts = pysftp.CnOpts()
-cnopts.hostkeys.add(hostname, key_type, key)
+CNOPTS = CnOpts()
+CNOPTS.hostkeys.add(HOSTNAME, KEY_TYPE, KEY)
 
 #------OPEN CONNECTION------#
-with pysftp.Connection(host=hostname, username=username, password=password, cnopts=cnopts) as sftp:
+with Connection(host=HOSTNAME, username=USERNAME, password=PASSWORD, cnopts=CNOPTS) as sftp:
 
     # Switch to a remote directory
-    print(remote_path)
-    sftp.cwd(remote_path)
+    sftp.cwd(REMOTE_PATH)
 
     # Obtain structure of the remote directory
     directory_structure = sftp.listdir_attr()
