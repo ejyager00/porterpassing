ca2dab18e4dd05c48b216c5ec6f585a5128eb737
Eric Yager <eric.yager@valpo.edu>
2020-02-04 14:31:16 -0600

    added additional data analysis

--- src/data/raster_reader.py
+++ src/data/raster_reader.py
@@ -1,10 +1,34 @@
 import os
 import rasterio
+import rasterio.features
+import rasterio.plot
+import numpy as np
 from data_accessor import open_sftp_connection
 
-with rasterio.open('') as dataset:
-    mask = dataset.dataset_mask()
+files = os.listdir('data/raw')
 
-    for geom, val in rasterio.features.shapes(mask, transform=dataset.transform):
-        geom = rasterio.warp.transform_geom(dataset.crs, 'EPSG:4326', geom, precision=6)
-        print(geom)
+for filename in files:
+    if filename[-4:]=='.tif':
+        print("\n")
+        print(filename)
+        with rasterio.open('data/raw/' + filename) as dataset:
+            mask = dataset.dataset_mask()
+
+            for geom, val in rasterio.features.shapes(mask, transform=dataset.transform):
+                geom = rasterio.warp.transform_geom(dataset.crs, 'EPSG:4326', geom, precision=6)
+                print(geom)
+
+            array = dataset.read()
+            stats = []
+
+            for band in array:
+                stats.append({
+                'min': band.min(),
+                'mean': band.mean(),
+                'median': np.median(band),
+                'max': band.max()})
+
+            for thing in stats:
+                print(thing)
+
+            rasterio.plot.show(dataset)
