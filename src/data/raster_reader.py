03a85ed2a13fb0df893ea0e960e95225eb57955b
Eric Yager <eric.yager@valpo.edu>
2020-02-04 13:35:06 -0600

    brief change to raster_reader.py

--- src/data/raster_reader.py
+++ src/data/raster_reader.py
@@ -2,7 +2,7 @@ import os
 import rasterio
 from data_accessor import open_sftp_connection
 
-with rasterio.open(attr.filename) as dataset:
+with rasterio.open('') as dataset:
     mask = dataset.dataset_mask()
 
     for geom, val in rasterio.features.shapes(mask, transform=dataset.transform):
