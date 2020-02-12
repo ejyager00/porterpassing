3f2b7863b116e755abfc4e62a4005a46843ae4a6
Eric Yager <eric.yager@valpo.edu>
2020-02-11 19:00:01 -0600

    haaaaaaaaaands

--- src/data/laspy_testing.py
+++ src/data/laspy_testing.py
@@ -2,16 +2,23 @@ from laspy.file import File
 import numpy as np
 
 las_file = File('data/raw/in2018_29502285_12.las')
-#las_file.open()
 
-
-#for x in las_file.get_points():
-#    print(x)
-
-print(len(las_file.get_points()))
-print(las_file.get_points()[0])
+print('scale ' + str(las_file.get_header().get_scale()))
+print('offset ' + str(las_file.get_header().get_offset()))
+print(str(len(las_file.get_points()))+' points')
+for dim in las_file.point_format:
+    print(dim.name)
+#for point in las_file.get_points():
+#    print(point)
+#las_file.visualize()
 coords = np.vstack((las_file.x, las_file.y, las_file.z)).transpose()
+found = []
 for x in coords:
-    print(x)
+    if x[0] not in found:
+        found.append(x[0])
+        print(len(found))
+print(len(found))
+print(found)
+#print(coords[1,1])
 
 las_file.close()
