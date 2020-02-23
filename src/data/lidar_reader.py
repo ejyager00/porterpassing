b3c29153327307a87eecfee36ccf6d87b5353ed4
Eric Yager <eric.yager@valpo.edu>
2020-02-23 11:14:19 -0600

    refactor lidar reader

--- src/data/lidar_reader.py
+++ src/data/lidar_reader.py
@@ -4,20 +4,22 @@ import numpy as np
 from scipy.interpolate import LinearNDInterpolator
 import pickle
 
-files = os.listdir('data/raw/')
-las_file_names = []
-for filename in files:
-    if filename[-4:]=='.las':
-        las_file_names.append(filename)
-las_file = File('data/raw/'+las_file_names.pop(0))
-coords = np.vstack((las_file.x, las_file.y, las_file.z)).transpose()
-las_file.close()
-for filename in las_file_names:
-    las_file = File('data/raw/'+filename)
-    coords = np.concatenate((coords, np.vstack((las_file.x, las_file.y, las_file.z)).transpose()), axis=0)
+def create_interpolator(dir='data/raw/',dumpfile='linear_interpolator.pickle'):
+    files = os.listdir(dir)
+    las_file_names = []
+    for filename in files:
+        if filename[-4:]=='.las':
+            las_file_names.append(filename)
+    las_file = File(dir+las_file_names.pop(0))
+    coords = np.vstack((las_file.x, las_file.y, las_file.z)).transpose()
     las_file.close()
-print(coords[:,0:2])
-print(coords[:,2])
-interpolator = LinearNDInterpolator(coords[:,0:2], coords[:,2])
-with open('linear_interpolator.pickle', 'wb') as pickle_file:
-    pickle.dump(interpolator, pickle_file)
+    for filename in las_file_names:
+        las_file = File(dir+filename)
+        coords = np.concatenate((coords, np.vstack((las_file.x, las_file.y, las_file.z)).transpose()), axis=0)
+        las_file.close()
+    interpolator = LinearNDInterpolator(coords[:,0:2], coords[:,2])
+    with open(dumpfile, 'wb') as pickle_file:
+        pickle.dump(interpolator, pickle_file)
+
+if __name__ == '__main__':
+    create_interpolator()
