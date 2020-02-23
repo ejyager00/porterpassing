4fe27e8f95824c39ebf09752df22fe330ea893dd
Eric Yager <eric.yager@valpo.edu>
2020-02-23 12:16:20 -0600

    refactor

--- src/data/centerline_reader.py
+++ src/data/centerline_reader.py
@@ -2,7 +2,7 @@ import shapefile
 
 def get_centerlines(dir="data/raw/Road_Centerlines/RoadExport"):
     with shapefile.Reader(dir) as sf:
-        return(sf.shapes())
+        return sf.shapes()
 
 if __name__ == '__main__':
     get_centerlines()
