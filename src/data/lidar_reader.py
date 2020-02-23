d64162e1ddbd4ff7579e8616a2e94a126deb7de9
Eric Yager <eric.yager@valpo.edu>
2020-02-23 10:57:26 -0600

    refactor

--- src/data/lidar_reader.py
+++ src/data/lidar_reader.py
@@ -9,7 +9,6 @@ las_file_names = []
 for filename in files:
     if filename[-4:]=='.las':
         las_file_names.append(filename)
-
 las_file = File('data/raw/'+las_file_names.pop(0))
 coords = np.vstack((las_file.x, las_file.y, las_file.z)).transpose()
 las_file.close()
@@ -17,6 +16,8 @@ for filename in las_file_names:
     las_file = File('data/raw/'+filename)
     coords = np.concatenate((coords, np.vstack((las_file.x, las_file.y, las_file.z)).transpose()), axis=0)
     las_file.close()
+print(coords[:,0:2])
+print(coords[:,2])
 interpolator = LinearNDInterpolator(coords[:,0:2], coords[:,2])
 with open('linear_interpolator.pickle', 'wb') as pickle_file:
     pickle.dump(interpolator, pickle_file)
