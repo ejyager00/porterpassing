d2658f5354c14a0dafff70c6fec49961695744e3
Eric Yager <eric.yager@valpo.edu>
2020-04-10 11:49:15 -0500

    update road binner

--- src/features/road_binner.py
+++ src/features/road_binner.py
@@ -1,11 +1,14 @@
 import shapefile
 import pickle
+import sys
+
+DEFAULT_BIN_LENGTH=5
 
 def get_centerline_data(dir="data/interim/centerlines"):
     with shapefile.Reader(dir) as sf:
         return sf.shapes()
 
-def generate_bins(road):
+def generate_bins(road, bin_length):
     rdpts=road.points
     segments=[]
     for i in range(1,len(rdpts)):
@@ -18,11 +21,11 @@ def generate_bins(road):
         dy=dy/magnitude
         curx = rdpts[i-1][0]
         cury = rdpts[i-1][1]
-        iterations=int(magnitude/6)
-        if magnitude%6<4:
+        iterations=int(magnitude/bin_length)
+        if magnitude%bin_length<bin_length*.65:
             iterations-=1
             if iterations<0:
-                diff = 4-magnitude
+                diff = bin_length*.65-magnitude
                 curx = curx - diff*dx
                 cury = cury - diff*dy
         bins=[]
@@ -46,10 +49,38 @@ def generate_bins(road):
         segments.append(bins)
     return segments
 
-roads = get_centerline_data()
+def check_arguments(args):
+    if len(args)<2:
+        raise RuntimeError("You must include a shapefile and a pickle file in the arguments.")
+    else
+        try:
+            shapefile.Reader(args[0])
+        except:
+            raise RuntimeError("The first argument should be a shapefile.")
+        try:
+            if (args[1][-7:]!=".pickle"):
+                raise RuntimeError("The second argument should be a pickle file.")
+        except:
+            raise RuntimeError("The second argument should be a pickle file.")
+        if len(args)>2:
+            try:
+                float(args[2])
+            except:
+                raise RuntimeError("The third argument should be a number.")
 
-road_bins = []
-for road in roads:
-    road_bins.append(generate_bins(road))
-with open("data/interim/roadbins.pickle", 'wb') as pickle_file:
-    pickle.dump(road_bins, pickle_file)
+def main(args):
+    check_arguments(args)
+    if len(args)<3:
+        bin_length=DEFAULT_BIN_LENGTH
+    else:
+        bin_length=float(args[2])
+    roads = get_centerline_data(args[0])
+
+    road_bins = []
+    for road in roads:
+        road_bins.append(generate_bins(road, bin_length))
+    with open(args[1], 'wb') as pickle_file:
+        pickle.dump(road_bins, pickle_file)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
